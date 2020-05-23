c030eb86b4777bc9349e31c4e1587fe4ac51d70a
Alexander Ronald Altman <alexanderaltman@me.com>
2020-05-23 14:13:17 -0700

    Getting started

--- mx.SatNQClause/suite.py
+++ mx.SatNQClause/suite.py
@@ -1,12 +1,39 @@
 suite = {
-  "name" : "SatNQClause",
-  "mxversion" : "5.263.8",
-  "imports" : {
-    "suites": [
-    ]
-  },
-  "libraries" : {
-  },
-  "projects" : {
-  },
+    "name": "SatNQClause",
+    "mxversion": "5.263.8",
+    "groupId": "com.pthariensflame.satnq-clause",
+    "version": "20.2.0",
+    "release": False,
+    "developer": {
+        "name": "Alexander Ronald Altman",
+        "email": "alexanderaltman@me.com",
+    },
+    "scm": {
+        "url": "https://github.com/pthariensflame/SatNQClause",
+        "read": "https://github.com/pthariensflame/SatNQClause.git",
+        "write": "git@github.com:pthariensflame/SatNQClause.git",
+    },
+    "defaultLicense": "BSD-new",
+
+    "imports": {
+        "suites": [
+        ]
+    },
+
+    "libraries": {
+    },
+
+    "projects": {
+        "com.pthariensflame.satnq-clause": {
+            "javaCompliance": "8",
+        },
+        "com.pthariensflame.satnq-clause.jdk9": {
+            "javaCompliance": "9+",
+            "multiReleaseJarVersion": "9",
+            "overlayTarget": "com.pthariensflame.satnq-clause",
+            "dependencies": [
+                "com.pthariensflame.satnq-clause",
+            ],
+        },
+    },
 }
